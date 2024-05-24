56871b37f1c96b8061ab3cafd2003b5d64e4608c
Karan Chawla <56110326+karanrchawla@users.noreply.github.com>
2024-05-23 20:47:58 -0700

    Delete src directory

--- src/router/Routers.tsx
+++
@@ -1,6 +0,0 @@
-import Home from "../pages/Home";
-
-const router = () => {
-  return [{ path: "/", element: <Home /> }];
-};
-export default router;
--- src/routers/apis/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import dataRouter from "./DataRouter";
-
-
-const router = express.Router();
-
-router.use('/data', dataRouter)
-
-export default router;
